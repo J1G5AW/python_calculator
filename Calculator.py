696b5edb2981661a80fc7c05556ca63ee63be9ea
J1G5AW <67883052+J1G5AW@users.noreply.github.com>
2020-07-06 08:26:18 +0530

    Create Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,20 @@
+#python 3.7.1
+
+import time
+print("this project was created by .\n(decimals can be used) ")
+print(" ")
+num1 = float(input("enter the first number : "))
+operator = input("enter the operation(+ or - or × or ÷) : ")
+num2 = float(input("enter the second number : "))
+print(" ")
+time.sleep(0.5)
+if operator == "+":
+  print(num1 + num2)
+elif operator == "-":
+  print(num1 - num2)
+elif operator == "×":
+  print(num1 * num2)
+elif operator == "÷":
+  print(num1 / num2)
+else:
+  print("Invalid operation! please choose from the given symbols.")
